304aab1326bea033aaba372fcc0e9b5b6a1efc76
agmbabazi <a.mbabazi3@alustudent.com>
2025-06-06 07:44:17 -0700

    web scraping revised

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-// This script reads and prints the content of a file
+// Script that reads and prints the content of a file in utf-8
 
 const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf8', function (err, data) {
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,12 +3,13 @@
 
 const fs = require('fs');
 
+// Get the file path and string to write from command-line arguments
 const filePath = process.argv[2];
 const content = process.argv[3];
 
+// Write the content to the file using UTF-8 encoding
 fs.writeFile(filePath, content, 'utf8', function (err) {
   if (err) {
-    console.log(err);
+    console.log(err); // Print the error if something went wrong
   }
 });
-
