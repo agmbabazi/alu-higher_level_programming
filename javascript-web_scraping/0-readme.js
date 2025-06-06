6d45d9c5883496306f9b249d713a4d2a766b7acb
agmbabazi <a.mbabazi3@alustudent.com>
2025-06-06 08:01:55 -0700

    readme file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,10 @@
-#!/usr/bin/env node
-// Script that reads and prints the content of a file in utf-8
+#!/usr/bin/node
 
 const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf8', (err, data) => {
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
